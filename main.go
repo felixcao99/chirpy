32895c009ad8afe6929da3e24822344077a31851
felixcao99 <felixcao99@hotmail.com>
2025-08-23 17:45:44 -0700

    filesever png

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
 	serverMux := http.NewServeMux()
+	serverMux.Handle("/assets/", http.FileServer(http.Dir(".")))
 	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	var server http.Server
 	server.Addr = ":8080"
 	server.Handler = serverMux
